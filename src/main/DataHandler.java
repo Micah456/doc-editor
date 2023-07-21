4b3145c8993135e483056d9f0a1e6e74e9634907
Micah456 <98129716+Micah456@users.noreply.github.com>
2023-07-21 14:51:41 +0100

    Implemented open file
    Added field currFile (File) to dataHandler.
    Implemented open
    Moved updateCounts logic into method called updateCounts
    Opening file calls updateCounts()
    File name added to title in docFrame

--- src/main/DataHandler.java
+++ src/main/DataHandler.java
@@ -1,8 +1,15 @@
 package main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class DataHandler {
+	protected File currFile; 
+	//protected String currFileName;
+	//protected String currDirectory;
 	protected boolean saveFile(String text, String path ) {
 		path = appendFileExt(path);
 		System.out.println("Path: " + path);
@@ -18,6 +25,26 @@ public class DataHandler {
             System.out.println(e.getMessage());
             return false;
         }
+	}
+	protected String readFile(File file) {
+		StringBuilder sb = new StringBuilder();
+		try {
+			FileReader fr = new FileReader(file);
+			int i;
+			while ((i = fr.read()) != -1) {
+                sb.append((char)i);
+            }
+			fr.close();
+			this.currFile = file;
+			return sb.toString();
+		} catch (FileNotFoundException fe) {
+			// TODO Auto-generated catch block
+			System.out.println("Error: File not found");
+			return null;
+		} catch(IOException ie) {
+			System.out.println("Error: Error while reading file");
+			return null;
+		}
 	}
 	/**
 	 * Returns String path with .txt appended if it doesn't already have this extension
