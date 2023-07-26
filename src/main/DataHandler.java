5a5a5cc28933d0065bf3c27be86ec3dd84c721c3
Micah456 <98129716+Micah456@users.noreply.github.com>
2023-07-26 12:11:02 +0100

    Implement delete

--- src/main/DataHandler.java
+++ src/main/DataHandler.java
@@ -1,8 +1,5 @@
 package main;
 
-import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -10,6 +7,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
 
 public class DataHandler {
 	protected File currFile; 
@@ -101,7 +99,14 @@ public class DataHandler {
 		textPane.cut();
 	}
 	protected void deleteSelected(JTextPane textPane) {
-		System.out.println("To implement delete selected");
-		
+		try {
+			int startIndex = textPane.getSelectionStart();
+			int len = textPane.getSelectionEnd()- startIndex;
+			textPane.getDocument().remove(startIndex, len);
+			
+		} catch (BadLocationException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
 	}
 }
