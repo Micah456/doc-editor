a43fb1fe120b69569b62e099408983b21c898673
Micah456 <98129716+Micah456@users.noreply.github.com>
2023-08-01 15:37:10 +0100

    SEM Tests Implemented

--- src/document/SpellingErrorManager.java
+++ src/document/SpellingErrorManager.java
@@ -7,18 +7,47 @@ import java.util.HashMap;
 
 public class SpellingErrorManager {
 	private HashMap<String,ArrayList<SpellingError>> spellingErrorMap;
+	private Comparator<SpellingError> errorComparator;
 	public SpellingErrorManager() {
 		this.spellingErrorMap = new HashMap<>();
+		this.errorComparator = getComparator();
 	}
 	public void clearMap() {
 		this.spellingErrorMap.clear();
 	}
-	public void addError(String word, int[] position) {
-		
+	/**
+	 * Adds a SpellingError to the SpellingErrorManager. 
+	 * @param word String that represents the error
+	 * @param position int[] referring to the position of the error in the text
+	 * @return true if added successfully, false if already exists or
+	 * if any of the arguments are invalid (including blank and null values)
+	 */
+	public boolean addError(String word, int[] position) {
+		if(word == null || word.isBlank()|| position == null) {
+			System.out.println("Invalid arguments.");
+			return false;
+		}
+		ArrayList<SpellingError> errors = this.spellingErrorMap.get(word);
+		if(errors != null) {
+			for(SpellingError e : errors) {
+				if(e.getPosition() == position) {
+					return false;
+				}
+			}
+			errors.add(new SpellingError(word,position));
+			return true;
+		}
+		else {
+			ArrayList<SpellingError> newErrorList = new ArrayList<>();
+			newErrorList.add(new SpellingError(word,position));
+			this.spellingErrorMap.put(word, newErrorList);
+			return true;
+		}
 	}
 	public int[] getLatestPosition(String word) {
 		if(spellingErrorMap.containsKey(word)) {
 			ArrayList<SpellingError> errors = spellingErrorMap.get(word);
+			errors.sort(errorComparator);
 			return errors.get(errors.size()-1).getPosition();
 		}
 		else return new int[] {-1,-1};
@@ -30,7 +59,18 @@ public class SpellingErrorManager {
 			errors.addAll(spellingErrorMap.get(key));
 		}
 		//Sort spelling errors by startPosition
-		errors.sort(new Comparator<SpellingError>() {
+		errors.sort(errorComparator);
+		return errors;
+	}
+	public int getCount() {
+		int count = 0;
+		for(String key : this.spellingErrorMap.keySet()) {
+			count+=this.spellingErrorMap.get(key).size();
+		}
+		return count;
+	}
+	private Comparator<SpellingError> getComparator() {
+		return new Comparator<SpellingError>() {
 
 			@Override
 			public int compare(SpellingError o1, SpellingError o2) {
@@ -46,15 +86,13 @@ public class SpellingErrorManager {
                 }
 			}
 			
-		});
-		return errors;
+		};
 	}
 	public class SpellingError{
 		private String word;
 		private ArrayList<String> topAlternatives;
 		private int[] position;
-		private int occurrance;
-		protected SpellingError(String word, int[] position, int occurrance) {
+		protected SpellingError(String word, int[] position) {
 			this.word = word;
 			this.position = position;
 			this.topAlternatives = getAlternatives();
--- src/test/TestSpellingErrorManager.java
+++ src/test/TestSpellingErrorManager.java
@@ -1,33 +1,118 @@
 package test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 
+import org.junit.Before;
 import org.junit.Test;
 import document.SpellingErrorManager;
+import document.SpellingErrorManager.SpellingError;
 
 public class TestSpellingErrorManager {
-	private final ArrayList<String> dict = getDictionary();
+	private SpellingErrorManager sem;
+	
+	@Before
+	public void setUp() {
+		sem = new SpellingErrorManager();
+	}
 	
 	@Test
-	public void testClearMap() {
-		fail();
+	public void testClearMap() { 
+		assertEquals(0, sem.getCount());
+		sem.addError("Hello", new int[] {3,8});
+		sem.addError("you", new int[] {9,11});
+		assertEquals(2, sem.getCount());
+		sem.clearMap();
+		assertEquals(0, sem.getCount());
+		
+	}
+	@Test
+	public void testGetCount() {
+		assertEquals(0, sem.getCount());
+		sem.addError("Hello", new int[] {3,8});
+		sem.addError("you", new int[] {9,11});
+		assertEquals(2, sem.getCount());
 		
 	}
 	@Test
 	public void testAddError() {
-		fail();
+		String word = "Hello";
+		int[] intArr1 = new int[] {3,8};
+		int[] intArr2 = new int[] {10,15};
 		
+		//Add successfully
+		assertTrue(sem.addError(word, intArr1));
+		ArrayList<SpellingError> errors = sem.getSpellingErrors();
+		assertEquals(1, errors.size());
+		assertEquals(word, errors.get(0).getWord());
+		assertEquals(intArr1, errors.get(0).getPosition());
+		
+		//Word already exists
+		assertTrue(sem.addError(word, intArr2));
+		errors = sem.getSpellingErrors();
+		assertEquals(2, errors.size());
+		assertEquals(intArr2, errors.get(1).getPosition());
+		
+		//Word and position already exists
+		assertFalse(sem.addError(word, intArr1));
+		assertEquals(2, sem.getCount());
+		
+		//Invalid parameters
+		assertFalse(sem.addError("", intArr1));
+		assertFalse(sem.addError(null, intArr1));
+		assertFalse(sem.addError(word, null));
+		assertEquals(2, sem.getCount());
 	}
 	@Test
 	public void testGetLatestPosition() {
-		fail();
-		
+		String error = "Error";
+		String panda = "Panda";
+		int[] intArr1 = new int[] {3,8}; // first
+		int[] intArr2 = new int[] {17,23}; // last
+		int[] intArr3 = new int[] {10,15}; // middle
+		int[] notFoundintArr = new int[] {-1,-1}; // not found
+		//Single item
+		sem.addError(error, intArr1);
+		assertEquals(1, sem.getCount());
+		assertEquals(intArr1, sem.getLatestPosition(error));
+		//Two items
+		sem.addError(error, intArr2);
+		assertEquals(2, sem.getCount());
+		assertEquals(intArr2, sem.getLatestPosition(error));
+		//Three items
+		sem.addError(error, intArr3);
+		assertEquals(3, sem.getCount());
+		assertEquals(intArr2[0], sem.getLatestPosition(error)[0]);
+		assertEquals(intArr2[1], sem.getLatestPosition(error)[1]);
+		//Doesn't exist
+		assertEquals(notFoundintArr[0], sem.getLatestPosition(panda)[0]);
+		assertEquals(notFoundintArr[1], sem.getLatestPosition(panda)[1]);
 	}
 	@Test
 	public void testGetSpellingErrors() {
-		fail();
+		String error = "Error";
+		String panda = "Panda";
+		int[] intArr1 = new int[] {17,23}; // third
+		int[] intArr2 = new int[] {25,30}; // last
+		int[] intArr3 = new int[] {10,15}; // second
+		int[] intArr4 = new int[] {3,8}; // first
+		sem.addError(error, intArr1); // third
+		sem.addError(error, intArr2); // last
+		sem.addError(panda, intArr3); // second
+		sem.addError(error, intArr4); // first
+		ArrayList<SpellingError> errors = sem.getSpellingErrors();
+		assertEquals(4, errors.size());
+		assertEquals(error, errors.get(0).getWord());
+		assertEquals(intArr4, errors.get(0).getPosition());
+		assertEquals(panda, errors.get(1).getWord());
+		assertEquals(intArr3, errors.get(1).getPosition());
+		assertEquals(error, errors.get(2).getWord());
+		assertEquals(intArr1, errors.get(2).getPosition());
+		assertEquals(error, errors.get(3).getWord());
+		assertEquals(intArr2, errors.get(3).getPosition());
 		
 	}
 	/*@Test
@@ -35,14 +120,4 @@ public class TestSpellingErrorManager {
 		//TODO To implement getAlternatives before running this test
 		fail();
 	}*/
-	
-	private ArrayList<String> getDictionary(){
-		ArrayList<String> dict = new ArrayList<>();
-		dict.add("hello");
-		dict.add("my");
-		dict.add("name");
-		dict.add("is");
-		dict.add("bob");
-		return dict;
-	}
 }
